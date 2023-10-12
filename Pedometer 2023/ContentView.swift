544d8b0f38de0e2bd69d4bcc794c756fb8c75f96
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-10-12 14:55:46 -0400

    Initial Commit

---
+++ Pedometer 2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pedometer 2023
+//
+//  Created by Michael Prenez-Isbell on 10/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pedometer 2023/Pedometer_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Pedometer_2023App.swift
+//  Pedometer 2023
+//
+//  Created by Michael Prenez-Isbell on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Pedometer_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
